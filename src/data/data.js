2a2cd7ae7243f9f20312d2913dcc3cededd79fae
Emre Doğramacı <emredogramaci91@gmail.com>
2023-02-02 13:57:19 +0300

    new work netflix clone  added

--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,6 @@
 import WorkImg from '../assets/projects/workImg.jpeg'
 import weatherApp from '../assets/projects/weatherApp.jpg'
+import cloneflix from '../assets/projects/cloneflix.jpg'
 
 export const data=[
     {
@@ -11,6 +12,13 @@ export const data=[
     },
     {
         id:"2",
+        name:"React Netflix Clone",
+        image:cloneflix,
+        github:"https://github.com/emredogramaci/React-Netflix-Clone",
+        live:"https://emreflix.netlify.app"
+    },
+    {
+        id:"3",
         name:"React Weather Forecast",
         image:weatherApp,
         github:"https://github.com/emredogramaci/React-Weather-Forcast-App",
